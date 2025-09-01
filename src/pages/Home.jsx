4ffa0dffda8e9253919c0ded16e655bb86bcf401
Moeez Rashid <moeezrashidd@gmail.com>
2025-09-01 14:25:49 -0700

    solved issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Footer from '../components/footer'
 const Home = () => {
     return (
     <>
         <Navbar />
         <Hero />
+        <Footer />
     </>
     )
 }
